4a785bdca77f809a9f069e3511eb95bf48d653ec
Francisco Barquero <fbarquero86@gmail.com>
2015-09-02 16:26:20 -0600

    chage task name to hit_example_com

--- locust_runner.py
+++ locust_runner.py
@@ -9,16 +9,18 @@ def execute_cli_command(commands):
     :param commands: list of command lines to execute in a list object
     :return:
     """
-    cmd = ""
-    for command in commands:
-        cmd += str("{};".format(command))
-    process = subprocess.Popen(cmd, shell=True)
-    process.communicate()
-    output = process.communicate()[0]
-    exit_code = process.returncode
+    try:
+        cmd = ""
+        for command in commands:
+            cmd += str("{};".format(command))
+        process = subprocess.Popen(cmd, shell=True)
+        process.communicate()
+        output = process.communicate()[0]
+        exit_code = process.returncode
 
-    if exit_code != 0:
-        raise Exception(command, exit_code, output)
-    return output
-
+        if exit_code != 0:
+            raise Exception(command, exit_code, output)
+        return output
+    except Exception, e:
+        print e.message
 execute_cli_command(["locust -f locustfile.py -c 250 -r 250 --no-web --only-summary"])
