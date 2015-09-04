8266c8350b8b0c7c9c26b9f593b22c39d0a18342
Francisco Barquero <fbarquero86@gmail.com>
2015-09-04 13:48:52 -0600

    Refactor to use Locust on swPerf instead of multi-mechanize Task completed:      Refactor user session creation      Add proxy functionality      refactor custom runner TODO: Improve login __init__(self) method Add functionality to track transactions and latency Track that information and gather it from distributed executions

--- test_scenarios/sowatest/sowatest_request_blocked.py
+++ test_scenarios/sowatest/sowatest_request_blocked.py
@@ -2,6 +2,7 @@ from locust import HttpLocust, TaskSet, task
 from BeautifulSoup import BeautifulSoup
 
 from configs.config import LocustConfigs as locust_config
+from configs.config import ProxyConfigs as proxy_config
 from utilities import sw_user_management as usrs
 from sw_requests.sowatest import SowatestRequests
 from custom_runner import custom_runner as c_runner
@@ -18,29 +19,31 @@ print("\nswPerf config loaded sucessfully")
 
 class UserBehavior(TaskSet):
 
+    # @task
+    # def hit_sowatest(self):
+    #     try:
+    #         if locust_config.USE_PROXY:
+    #             user_credentials = users_pool.pop(0)
+    #             proxy_request = SowatestRequests(self.client)
+    #             response = proxy_request.sowatest_through_proxy(user_credentials)
+    #             # soup = BeautifulSoup(response.text)
+    #             # Assert Section
+    #             # assert response.status_code is 200, "unexpected response status code {}".format(response.status_code)
+    #             # assert "Access Denied" in str(soup.find("title").text)
+    #             # assert '<div id="blockedBanner">' in response.text
+    #         else:
+    #             print("no proxy")
+    #     except Exception, e:
+    #         if locust_config.USE_PROXY:
+    #             if e.message is 'location':
+    #                 raise Exception('Possible authentication error against IDP!')
+    #             raise
+    #     finally:
+    #         if locust_config.USE_PROXY:
+    #             users_pool.append(user_credentials)
     @task
-    def hit_sowatest(self):
-        try:
-            if locust_config.USE_PROXY:
-                user_credentials = users_pool.pop(0)
-                proxy_request = SowatestRequests(self.client)
-                response = proxy_request.sowatest_through_proxy(user_credentials)
-                # soup = BeautifulSoup(response.text)
-                # Assert Section
-                # assert response.status_code is 200, "unexpected response status code {}".format(response.status_code)
-                # assert "Access Denied" in str(soup.find("title").text)
-                # assert '<div id="blockedBanner">' in response.text
-            else:
-                print("no proxy")
-        except Exception, e:
-            if locust_config.USE_PROXY:
-                if e.message is 'location':
-                    raise Exception('Possible authentication error against IDP!')
-                raise
-        finally:
-            if locust_config.USE_PROXY:
-                users_pool.append(user_credentials)
-
+    def test_proxy(self):
+        self.client.get("/", proxies=proxy_config.PROXIES)
 
 
 class WebsiteUser(HttpLocust):
